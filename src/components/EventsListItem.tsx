ba21421cc885e82e06c79321f9e990fc3ea79cfb
Jay Kariesch <viamemo@gmail.com>
2022-02-02 19:21:35 -0600

    fix: colors

--- src/components/EventsListItem.tsx
+++ src/components/EventsListItem.tsx
@@ -12,32 +12,20 @@ type EventsListItem = HoursList & {
   renderEvent(event: Event | EventWithIntersection): JSX.Element;
 };
 
-const getBackgroundColorByEventType = (type: string | undefined) =>
-  type === "ATTACH_REPAIR_ORDER"
-    ? "#DC2127"
-    : type === "ORDER_PARTS"
-    ? "#FBD75B"
-    : type === "RECEIVE_PARTS"
-    ? "#FF887C"
-    : type === "LOADS_PARTS"
-    ? "#A4BDFC"
-    : type === "READY"
-    ? "#7AE7BF"
-    : type === "EN_ROUTE"
-    ? "#46D6DB"
-    : type === "IN_PROGRESS"
-    ? "white"
-    : type === "READY_FOR_BILLING"
-    ? "#51B749"
-    : type === "BILLING_ISSUE"
-    ? "#FFB878"
-    : type === "COMPLETE"
-    ? "#5484ED"
-    : type === "CANCELLED"
-    ? "#616161"
-    : type === "NO_SHOW"
-    ? "#FF8303"
-    : "#B74ED4";
+const COLORS: Record<string, string> = {
+  ATTACH_REPAIR_ORDER: "#DC2127",
+  ORDER_PARTS: "#FBD75B",
+  RECEIVE_PARTS: "#FF887C",
+  LOADS_PART: "#A4BDFC",
+  READY: "#7AE7BF",
+  EN_ROUTE: "#46D6DB",
+  IN_PROGRESS: "#fff",
+  READY_FOR_BILLING: "#51B749",
+  BILLING_ISSUE: "#FFB878",
+  COMPLETE: "#5484ED",
+  CANCELLED: "#616161",
+  NO_SHOW: "#FF8303",
+};
 
 export const EventsListItem: React.FC<EventsListItem> = ({
   events,
@@ -73,7 +61,7 @@ export const EventsListItem: React.FC<EventsListItem> = ({
         zIndex: 1,
         borderLeft: "6px solid #458ebb",
         borderBottom: "2px solid black",
-        background: getBackgroundColorByEventType(event.type),
+        background: COLORS[event.type] ?? "#B74ED4",
       }}
       className={`${classNames.event} ${classNames.type}`}
       title={event.name}
